de80ffd4f70a911d7c41b43322bf9fcdbfbe0d5e
sandeepml739 <92722206+sandeepml739@users.noreply.github.com>
2021-12-06 17:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,9 +239,9 @@ def upload_file():
     # write multiple rows
                 #writer.writerow(finalresult)
 
-        #return render_template("upload-files.html",msg="Files has been uploaded sucessfully",ll='C:/Users/sandeep.mandula/Downloads/flask/flask/final.zip')
-    #return render_template("upload-files.html",msg="Please Choose a files")
-    return jsonify({"msg":"Files has been uploaded sucessfully","ll":'C:/Users/sandeep.mandula/Downloads/flask/flask/final.zip'})
+        return render_template("upload-files.html",msg="Files has been uploaded sucessfully",ll='final.zip')
+    return render_template("upload-files.html",msg="Please Choose a files")
+    #return jsonify({"msg":"Files has been uploaded sucessfully","ll":'C:/Users/sandeep.mandula/Downloads/flask/flask/final.zip'})
 
 
 
